56ca06ba1c751b59cf43c7e1d1432d253d726823
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2024-03-13 13:49:16 +0600

    Lots of design changes

--- res/app.js
+++ res/app.js
@@ -1,3 +1,10 @@
+const tabButtonHanlder = (page) => {
+    $("#about").attr("class", (page == "about") ? "active" : "");
+    $("#skils").attr("class", (page == "skils") ? "active" : "");
+    $("#projects").attr("class", (page == "projects") ? "active" : "");
+    $("#education").attr("class", (page == "education") ? "active" : "");
+    $("#contact").attr("class", (page == "contact") ? "active" : "");
+};
 window.PREVIOUS_TAB = null;
 const getWaitTime = (page) => {
     if(null == page) page = window.PREVIOUS_TAB;
@@ -10,14 +17,27 @@ const getWaitTime = (page) => {
     if(null != times[page]) return times[page];
     else return 0;
 };
+window.TIMEOUT_PAGINATION = null;
+const hadlePagination = (page) => {
+    let pagination = $("#pagination");
+    pagination.css('display', 'block');
+    pagination.css('animation', 'fadeOutDown');
+    pagination.css('animation-duration', '3s');
+    if(null != window.TIMEOUT_PAGINATION) clearTimeout(window.TIMEOUT_PAGINATION);
+    window.TIMEOUT_PAGINATION = setTimeout(() => {
+        pagination.css('display', 'none');
+    }, 3000);
+};
 const initPage = (page) => {
     $.get('/pages/'+ page +'.html').done((data) => {
         setTimeout(()=> {
             $("#content").html(data);
             window.PREVIOUS_TAB = page;
+            hadlePagination(page);
         }, getWaitTime(null));
     });
     handleTabChangeAmination(page);
+    tabButtonHanlder(page);
 };
 $(document).ready(() => {
     initPage('about');
