c7d13dc3b2d418e30ae8dd2f2d347e70484ecafc
Md. Monoarul Islam Moon <64534598+immo2n@users.noreply.github.com>
2025-02-10 15:30:39 +0600

    Update python.js

--- packages/python.js
+++ packages/python.js
@@ -1,3 +1,3 @@
 const js = document.createElement('script');
-js.src = 'https://pyscript.net/latest/pyscript.js';
+js.src = 'https://cdn.jsdelivr.net/npm/pyscript@1.0.6/index.min.js';
 document.head.appendChild(js);
